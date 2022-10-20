b150472f52870cbcb8e25410a76fe7b1958e126a
chris <christopher.narayanan@uzh.ch>
2022-10-20 09:28:23 +0200

    testing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int x = 0;
         System.out.println("Hello world!");
         System.out.println(x);
+        System.out.println("testing");
         int z=0;
     }
 }
