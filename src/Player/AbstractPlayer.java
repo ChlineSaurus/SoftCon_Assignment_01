62dbff24ad74e51f3a028fb2f5419426c1d838cf
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-10-21 21:16:06 +0200

    General Methods abstract Player

---
+++ src/Player/AbstractPlayer.java
@@ -0,0 +1,19 @@
+package Player;
+
+abstract class AbstractPlayer {
+    //private Grid ownGrid() = new Grid;
+    //private Grid targetGrid() = new Grid;
+    //private fleet ownFleet() = new fleet;
+
+    boolean validBoatPosition() {
+        return true;
+    }
+
+    void shoot() {
+
+    }
+
+    void placeBoat() {
+
+    }
+}
---
+++ src/Player/type/ComputerPlayer.java
@@ -0,0 +1,4 @@
+package Player.type;
+
+public class ComputerPlayer {
+}
