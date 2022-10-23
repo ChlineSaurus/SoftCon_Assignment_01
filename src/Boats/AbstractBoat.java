4144f4076a950947022191e6a6fa259abdc5e958
chris <christopher.narayanan@uzh.ch>
2022-10-23 11:17:23 +0200

    ishit & issunk

--- src/Boats/AbstractBoat.java
+++ src/Boats/AbstractBoat.java
@@ -3,6 +3,14 @@ package Boats;
 public class AbstractBoat {
     // I'm a boat
     private int hitpoints;
+    public boolean issunk(){
+        return hitpoints == 0;
+    }
+    //in order that ishit is called one first must recieve the information from the grid make this function private sothat the it isn't absued
+    public void isHit(){
+        assert !issunk();
+        hitpoints--;
+    }
 
     static void toot() {
         System.out.println("Toooooooot");
