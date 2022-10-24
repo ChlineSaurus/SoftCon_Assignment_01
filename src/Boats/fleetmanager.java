2cd47ca65c55528df0292f691586760e8d5a7afb
chris <christopher.narayanan@uzh.ch>
2022-10-24 16:45:10 +0200

    allowedboats

--- src/Boats/fleetmanager.java
+++ src/Boats/fleetmanager.java
@@ -1,6 +1,12 @@
 package Boats;
 
 public class fleetmanager {
-    private int boats_not_sunk=6;
+    private int boats_not_sunk = 6;
     //boolean isFleetDestroyed
+
+    private int allowed_carriers = 1;
+    private int allowed_subs=3;
+    private int allowed_battleships=2;
+    private int allowed_patrol_boats=4;
+
 }
