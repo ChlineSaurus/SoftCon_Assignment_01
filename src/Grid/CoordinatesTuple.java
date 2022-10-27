3ecc3a56d9b16cfb4f1c75aa1bc22e3db99fc12c
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-10-27 11:19:43 +0200

    Container Class für Coordinate Tuple

---
+++ src/Grid/CoordinatesTuple.java
@@ -0,0 +1,12 @@
+package Grid;
+
+//Container Class such that we are able to give coordinate in pairs
+public class CoordinatesTuple {
+    final public Row row;
+    final public Column column;
+
+    public CoordinatesTuple(Row row, Column column) {
+        this.row = row;
+        this.column = column;
+    }
+}
