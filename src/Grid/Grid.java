76eee16606e230fd6a8a89e5ee5e37e66b4e03ef
daniel <daniel.maksimovic01@gmail.com>
2022-10-29 19:38:31 +0200

    ArrayList Matrix with GridCells in them

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -1,53 +1,47 @@
 package Grid;
 
+import java.util.ArrayList;
+
 import static java.lang.Math.abs;
 import static java.lang.Math.max;
+
 
 public class Grid {
 
     public Grid() {
 
     }
+    /*
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
+            [A,B,C,D,E,F,G,H,I,J],
 
-    ;
+     */
+    ArrayList<GridCell> rowList = new ArrayList<GridCell>();
+    ArrayList<ArrayList<GridCell>> gridList = new ArrayList<ArrayList<GridCell>>();
 
-
-    public void initializeGrid() {
-        enum grid {
-            enum Row0 {
-                A, B, C, D, E, F, G, I, J;
+    //create 2D-List with 10 rows and GridCells in them
+    public void newGrid() {
+        for (Row row : Row.values()) {
+            gridList.add(rowList = new ArrayList<GridCell>());
+            for (Column column : Column.values()) {
+                GridCell temp = new GridCell(row, column);
+                rowList.add(temp);
             }
-            enum Row1 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row2 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row3 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row4 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row5 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row6 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row7 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row8 {
-                A, B, C, D, E, F, G, I, J;
-            }
-            enum Row9 {
-                A, B, C, D, E, F, G, I, J;
-            };
         }
+    }
 
 
-        public int getDistance (GridCell a, GridCell b){
+
+
+
+        /*public int getDistance (GridCell a, GridCell b){
             int columDelta = abs(a.GetColumnIndex() - b.GetColumnIndex());
             int rowDelta = abs(a.GetRowIndex() - b.GetRowIndex());
             //this function to check if the distance of the user is valid for instance
@@ -56,6 +50,7 @@ public class Grid {
             }
             return max(columDelta, rowDelta);
 
-        }
-    }
+         */
+
 }
+
