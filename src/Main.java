0e4ea2e025ee68a627d49382867e304a1dc0b515
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-10-30 22:42:08 +0100

    Appassige vo de Spieler, und zrugstelle vom Main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 import Exceptions.BoatPlacement.BoatPositionOccupiedException;
 import Exceptions.IllegalShotException;
 import Grid.*;
-import Boats.FleetManager;
 import UI.Display;
 
 import static java.lang.System.exit;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        FleetManager fleet=new FleetManager();
-        System.out.println(fleet.BoatsToBePlaced());
-
     GameManager gameManager = GameManager.getInstance();
     gameManager.ManageGame();
     exit(0);
