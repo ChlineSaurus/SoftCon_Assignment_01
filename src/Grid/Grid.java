a0c912afa6bf8bfbf7553e5cb1b89541c394a31f
chris <christopher.narayanan@uzh.ch>
2022-10-31 22:29:24 +0100

    Revert "Display Winner"
    This reverts commit 87308c3fafa939ad0a4ac354955dca1efe042a13.

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -133,11 +133,6 @@ public class Grid{
         return new GridIterator(gridList, gridCellDisplayer);
     }
 
-
-
-
-
-
         /*public int getDistance (GridCell a, GridCell b){
             int columDelta = abs(a.GetColumnIndex() - b.GetColumnIndex());
             int rowDelta = abs(a.GetRowIndex() - b.GetRowIndex());
